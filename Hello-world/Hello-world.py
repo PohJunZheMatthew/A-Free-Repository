775b8d6dda88cb1d5ad8d2fc2cefaa171209f495
PohJunZheMatthew <poh_jun_zhe_matthew@s2025.ssts.edu.sg>
2025-03-16 19:03:28 +0800

    Update Hello-world.py
    Added tkinter,added tkinter message box

--- Hello-world/Hello-world.py
+++ Hello-world/Hello-world.py
@@ -7,3 +7,16 @@ sys.stdout.write("Hello world")
 #Write it into a file
 with open("Hello-world.txt","w+") as file:
   file.write("Hello world")
+#Say it as a tkinter GUI
+from tkinter import *
+root = Tk()
+label = Label(root,text="Hello world")
+label.pack()
+root.mainloop()
+#Say it as a tkinter messagebox GUI
+from tkinter import *
+from tkinter import messagebox
+messagebox.showinfo("showinfo", "Hello world") 
+#Say it through pygame
+#Make sure pygame is installed by using pip install pygame
+import pygame
