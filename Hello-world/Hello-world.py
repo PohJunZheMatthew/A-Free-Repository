ddfa153cc33cfccbe136deb5c880f3ce8c6b5f9d
PohJunZheMatthew <poh_jun_zhe_matthew@s2025.ssts.edu.sg>
2025-03-16 13:45:57 +0800

    Update Hello-world.py
    Added a another way to say hello world by writing it into a file

--- Hello-world/Hello-world.py
+++ Hello-world/Hello-world.py
@@ -1,6 +1,9 @@
-#Here a are some ways to say hello world in python
+#Here are some ways to say hello world in python
 #defult print() function
 print("Hello world")
 #Use system standard output
 import sys
 sys.stdout.write("Hello world")
+#Write it into a file
+with open("Hello-world.txt","w+") as file:
+  file.write("Hello world")
