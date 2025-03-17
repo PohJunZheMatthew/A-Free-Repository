277543948f618cf4bb29ddc3e2fa4cba0e2488d0
PohJunZheMatthew <poh_jun_zhe_matthew@s2025.ssts.edu.sg>
2025-03-17 08:49:22 +0800

    Update Hello-world.py
    Added turtle and finished pygame

--- Hello-world/Hello-world.py
+++ Hello-world/Hello-world.py
@@ -7,6 +7,9 @@ sys.stdout.write("Hello world")
 #Write it into a file
 with open("Hello-world.txt","w+") as file:
   file.write("Hello world")
+#Say it in turtle
+import turtle
+turtle.write("Hello world")
 #Say it as a tkinter GUI
 from tkinter import *
 root = Tk()
@@ -19,4 +22,17 @@ from tkinter import messagebox
 messagebox.showinfo("showinfo", "Hello world") 
 #Say it through pygame
 #Make sure pygame is installed by using pip install pygame
-import pygame
+import pygame,sys
+pygame.init()
+window = pygame.display.set_mode((500,500))
+font = pygame.font.Font('Arial.ttf',32)
+text = font.render("Hello world",True,(0,0,0))
+textRect = text.get_rect()
+while True:
+  window.fill((255,255,255))
+  window.blit(text,textRect)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit(0)
+  pygame.display.update()
