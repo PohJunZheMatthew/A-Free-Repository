4ab5ac1d69ff440d7ba549f311d07f19a05e9643
PohJunZheMatthew <poh_jun_zhe_matthew@s2025.ssts.edu.sg>
2025-03-20 10:32:36 +0800

    Update Hello-world(Java-awt-graphics).java
    fixed the issue where the frame cannot be dispose.

--- Hello-world/Hello-world(Java)/Hello-world(Java-awt-graphics).java
+++ Hello-world/Hello-world(Java)/Hello-world(Java-awt-graphics).java
@@ -3,6 +3,13 @@ public class Main extends Frame {
     public Main(){
         setVisible(true);
         setSize(100,100);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+                System.exit(0);
+            }
+        });
     }
     @Override
     public void paint(Graphics g){
