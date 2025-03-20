8095cc0abaa80285fb018c5abd6ff9a06929f78d
PohJunZheMatthew <poh_jun_zhe_matthew@s2025.ssts.edu.sg>
2025-03-20 09:00:48 +0800

    N-th Prime Number python solution

---
+++ N-th Prime Number/N-th Prime Number.py
@@ -0,0 +1,25 @@
+import sys,math
+data = sys.stdin.read()
+data = data.split("\n")
+indexOfPrimeNums = int(data[0])
+primeNums = []
+currentNum = 2
+def isPrime(n):
+    is_prime = True
+    if n <= 1:
+        is_Prime = False
+    else:
+        is_prime = True
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                is_prime = False
+                break
+    return is_prime
+while True:
+    if len(primeNums) == indexOfPrimeNums:
+        print(primeNums[len(primeNums)-1])
+        break
+    else:
+        if isPrime(currentNum):
+            primeNums.append(currentNum)
+        currentNum+=1;
